e2fc6fe111b7079f7b47c717cbf511f99a41825a
Alex Rodriguez <alex.rodriguezlopez@gmail.com>
2013-11-21 01:02:33 +0100

    Readme

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
-bitcoin_price_alert
+Bitcoin_price_alert
 ===================
 
-Bitcoin price alert
+Bitcoin_price_alert searches for the current price of the bitcoin currency and sends an alert through email if it reaches the min_alert_value/max_alert_value. 
+
+http://api.bitcoincharts.com/ is been used to gather data, Bitcoincharts ask not to query more often than 15 minutes, so if you schedule this script in crontab be aware of this.
+
+Installing
+----------
+Use crontab or whatever you use to schedule jobs.
+
+Crontab example:
+
+PATH=$PATH:/usr/bin:/bin:/usr/local/bin
+# m h  dom mon dow   command
+  0,30 * * * * ruby /home/xyz/bitcoin_price_alert.rb 
+
+
+Usage
+-----
+```ruby
+#bcoin = BitCoin.new(currency,min_alert_value, max_alert_value, [fav_sites])
+
+#Initialize to search all sites
+bcoin = BitCoin.new('EUR',100, 700)
+
+#Initialize to search selected sites
+bcoin = BitCoin.new('EUR',100,700,['mtgoxEUR','crytrEUR'])
+
+#Start search
+bcoin.current_value
+```
+
+Customization
+------------
+# Currencies
+  # ["ARS", "AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GAU", "GBP", "HKD", "HUF", "ILS", "INR", "JPY", "LTC", "MXN", "NOK", "NZD", "PLN", "RUB", "SEK", "SGD", "SLL", "THB", "UAH", "USD", "XRP", "ZAR"]    
+
+# Fav Sites (optional)
+  # ["localbtcARS", "bitmarketAUD", "btcmarketsAUD", "cryptoxAUD", "localbtcAUD", "mtgoxAUD", "ruxumAUD", "thAUD", "wbxAUD", "weexAUD", "bbmBRL", "localbtcBRL", "mrcdBRL", "localbtcCAD", "lybitCAD", "mtgoxCAD", "virtexCAD", "weexCAD", "localbtcCHF", "mtgoxCHF", "ruxumCHF", "thCLP", "anxhkCNY", "btcnCNY", "mtgoxCNY", "rmbtbCNY", "bitcashCZK", "localbtcCZK", "localbtcDKK", "mtgoxDKK", "aqoinEUR", "b7EUR", "bcEUR", "bitcurexEUR", "bitmarketEUR", "btc24EUR", "btcdeEUR", "btceEUR", "btcexEUR", "crytrEUR", "fbtcEUR", "globalEUR", "imcexEUR", "intrsngEUR", "justEUR", "localbtcEUR", "mtgoxEUR", "rippleEUR", "rockEUR", "ruxumEUR", "thEUR", "vcxEUR", "bcmBMGAU", "bcmPXGAU", "bcGBP", "bitmarketGBP", "britcoinGBP", "globalGBP", "intrsngGBP", "localbtcGBP", "mtgoxGBP", "ruxumGBP", "anxhkHKD", "btchkexHKD", "localbtcHKD", "mtgoxHKD", "ruxumHKD", "ruxumHUF", "bit2cILS", "localbtcILS", "localbtcINR", "thINR", "btcexJPY", "mtgoxJPY", "ruxumJPY", "bitmeLTC", "justLTC", "localbtcMXN", "justNOK", "localbtcNOK", "bitnzNZD", "localbtcNZD", "mtgoxNZD", "bidxtrmPLN", "bitchangePLN", "bitcurexPLN", "bitmarketPLN", "bitomatPLN", "freshPLN", "globalPLN", "intrsngPLN", "localbtcPLN", "mtgoxPLN", "ruxumPLN", "bitmarketRUB", "btceRUR", "btcexRUB", "btcexWMR", "btcexYAD", "localbtcRUB", "mtgoxRUB", "ruxumRUB", "fybseSEK", "kptnSEK", "localbtcSEK", "mtgoxSEK", "ruxumSEK", "fybsgSGD", "localbtcSGD", "mtgoxSGD", "ruxumSGD", "rockSLL", "virwoxSLL", "localbtcTHB", "mtgoxTHB", "ruxumTHB", "ruxumUAH", "anxhkUSD", "b2cUSD", "b7USD", "bcmBMUSD", "bcmLRUSD", "bcmMBUSD", "bcmPPUSD", "bitboxUSD", "bitfloorUSD", "bitkonanUSD", "bitmarketUSD", "bitmeUSD", "bitstampUSD", "btc24USD", "btceUSD", "btcexUSD", "btcexWMZ", "btctreeUSD", "cbxUSD", "cryptoxUSD", "crytrUSD", "exchbUSD", "fbtcUSD", "globalUSD", "icbitUSD", "imcexUSD", "intrsngUSD", "justUSD", "localbtcUSD", "lybitUSD", "mtgoxUSD", "rippleUSD", "rockUSD", "ruxumUSD", "thLRUSD", "thUSD", "vcxUSD", "weexUSD", "justXRP", "rippleXRP", "snwcnXRP", "bitxZAR", "localbtcZAR", "ruxumZAR"]
+
+
+Contributing
+------------
+If you'd like to contribute, fork
+the project at [github](https://github.com/alex-rodriguez/bitcoin_price_alert), make your changes, 
+then send a pull request.
+
+Contact
+-------
+Comments and feedback are welcome.
+
+License
+-------
+This code is free to use under the terms of the MIT license.
+
+
---
+++ bitcoin_price_alert.rb
@@ -0,0 +1,76 @@
+require 'net/http'
+require 'json'
+require 'net/smtp'
+require 'mail'
+
+class BitCoin
+  attr_accessor :currency, :min_alert_value, :max_alert_value, :fav_sites
+
+  def initialize(currency, min_alert_value, max_alert_value, fav_sites=[])
+  	self.currency = currency
+  	self.min_alert_value = min_alert_value
+    self.max_alert_value = max_alert_value
+  	self.fav_sites = fav_sites 
+  end
+
+  def current_value
+    uri = URI.parse("http://api.bitcoincharts.com/v1/markets.json")
+    response = Net::HTTP.get_response(uri)
+    result = JSON.parse(response.body)
+    filtered_results = {}
+    
+    result.each do |data|  
+      if self.fav_sites.any?     
+		filtered_results[data['symbol']]=data['close'] if (fav_sites.include?(data['symbol']) && data['currency'] == self.currency && !(data['bid'].nil? || data['avg'].nil?))
+      else      	
+    	filtered_results[data['symbol']]=data['close'] if (data['currency'] == self.currency && !(data['bid'].nil? || data['avg'].nil?))
+      end    	    	
+    end
+
+    min_result = filtered_results.min_by{ |x,y| y}
+    max_result = filtered_results.max_by{ |x,y| y}
+   
+    if min_result[1] <=  self.min_alert_value
+      body_text =  "Your min price has been reached! -- Site: #{min_result[0].to_s} -- Price: #{min_result[1].to_s} ( #{self.currency} )" 
+      send_email(body_text) 
+    end
+    if max_result[1] >=  self.max_alert_value
+      body_text =  "Your max price has been reached! -- Site: #{max_result[0].to_s} -- Price: #{max_result[1].to_s} ( #{self.currency} )" 
+      send_email(body_text) 
+    end
+  rescue => e
+    puts "An error has occurred: #{e.message}"	
+  end
+
+  private
+  def send_email(body_text)  	
+  	options = { :address              => "smtp.gmail.com",
+            :port                 => 587,          
+            :user_name            => 'your-email@gmail.com',
+            :password             => 'password',
+            :authentication       => 'plain',
+            :enable_starttls_auto => true  }
+
+  	Mail.defaults do
+  	  delivery_method :smtp, options
+  	end
+	
+  	Mail.deliver do
+  	  to 'your-email@gmail.com'
+  	  from 'bitcoin@bitcoinsearcher.net'
+  	  subject 'Bitcoin price alert'
+  	  body body_text	  
+  	end			
+  end 
+end
+
+  # Currencies
+  # ["ARS", "AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GAU", "GBP", "HKD", "HUF", "ILS", "INR", "JPY", "LTC", "MXN", "NOK", "NZD", "PLN", "RUB", "SEK", "SGD", "SLL", "THB", "UAH", "USD", "XRP", "ZAR"]    
+
+  # Fav Sites
+  # ["localbtcARS", "bitmarketAUD", "btcmarketsAUD", "cryptoxAUD", "localbtcAUD", "mtgoxAUD", "ruxumAUD", "thAUD", "wbxAUD", "weexAUD", "bbmBRL", "localbtcBRL", "mrcdBRL", "localbtcCAD", "lybitCAD", "mtgoxCAD", "virtexCAD", "weexCAD", "localbtcCHF", "mtgoxCHF", "ruxumCHF", "thCLP", "anxhkCNY", "btcnCNY", "mtgoxCNY", "rmbtbCNY", "bitcashCZK", "localbtcCZK", "localbtcDKK", "mtgoxDKK", "aqoinEUR", "b7EUR", "bcEUR", "bitcurexEUR", "bitmarketEUR", "btc24EUR", "btcdeEUR", "btceEUR", "btcexEUR", "crytrEUR", "fbtcEUR", "globalEUR", "imcexEUR", "intrsngEUR", "justEUR", "localbtcEUR", "mtgoxEUR", "rippleEUR", "rockEUR", "ruxumEUR", "thEUR", "vcxEUR", "bcmBMGAU", "bcmPXGAU", "bcGBP", "bitmarketGBP", "britcoinGBP", "globalGBP", "intrsngGBP", "localbtcGBP", "mtgoxGBP", "ruxumGBP", "anxhkHKD", "btchkexHKD", "localbtcHKD", "mtgoxHKD", "ruxumHKD", "ruxumHUF", "bit2cILS", "localbtcILS", "localbtcINR", "thINR", "btcexJPY", "mtgoxJPY", "ruxumJPY", "bitmeLTC", "justLTC", "localbtcMXN", "justNOK", "localbtcNOK", "bitnzNZD", "localbtcNZD", "mtgoxNZD", "bidxtrmPLN", "bitchangePLN", "bitcurexPLN", "bitmarketPLN", "bitomatPLN", "freshPLN", "globalPLN", "intrsngPLN", "localbtcPLN", "mtgoxPLN", "ruxumPLN", "bitmarketRUB", "btceRUR", "btcexRUB", "btcexWMR", "btcexYAD", "localbtcRUB", "mtgoxRUB", "ruxumRUB", "fybseSEK", "kptnSEK", "localbtcSEK", "mtgoxSEK", "ruxumSEK", "fybsgSGD", "localbtcSGD", "mtgoxSGD", "ruxumSGD", "rockSLL", "virwoxSLL", "localbtcTHB", "mtgoxTHB", "ruxumTHB", "ruxumUAH", "anxhkUSD", "b2cUSD", "b7USD", "bcmBMUSD", "bcmLRUSD", "bcmMBUSD", "bcmPPUSD", "bitboxUSD", "bitfloorUSD", "bitkonanUSD", "bitmarketUSD", "bitmeUSD", "bitstampUSD", "btc24USD", "btceUSD", "btcexUSD", "btcexWMZ", "btctreeUSD", "cbxUSD", "cryptoxUSD", "crytrUSD", "exchbUSD", "fbtcUSD", "globalUSD", "icbitUSD", "imcexUSD", "intrsngUSD", "justUSD", "localbtcUSD", "lybitUSD", "mtgoxUSD", "rippleUSD", "rockUSD", "ruxumUSD", "thLRUSD", "thUSD", "vcxUSD", "weexUSD", "justXRP", "rippleXRP", "snwcnXRP", "bitxZAR", "localbtcZAR", "ruxumZAR"]
+
+bcoin = BitCoin.new('EUR',100, 700)
+#b = BitCoin.new('EUR',100,700,['mtgoxEUR','crytrEUR'])
+bcoin.current_value
+
